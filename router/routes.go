e0ef318261f9fb1c7d4f8d3199ce31e6e16490c0
Daniel Rocha <dgustavo@id.uff.br>
2023-03-19 17:23:45 -0300

    feat: group routes

--- router/routes.go
+++ router/routes.go
@@ -1,13 +1,22 @@
 package router
 
 import "github.com/gin-gonic/gin"
+import "net/http"
 
 func initializeRoutes(router *gin.Engine) {
-	print("chamei")
-	//definindo rota
+	// definindo rota OLD style
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	// definindo rota NEW style
+	v1 := router.Group("/api/v1"); {
+		v1.GET("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "GET Opening",
+			})
+		})
+	}
 }
