32c9281c32fc54dc3618dbae9bf2752757728897
Daniel Rocha <dgustavo@id.uff.br>
2023-03-20 01:09:49 -0300

    feat: add swagger documentation to server

--- router/routes.go
+++ router/routes.go
@@ -3,14 +3,20 @@ package router
 import (
 	"github.com/danielbrazrocha/gopportunities/handler"
 	"github.com/gin-gonic/gin"
+	docs "github.com/danielbrazrocha/gopportunities/docs"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
 
 	//Initialize Handler
 	handler.Init()
+
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
 	
-	v1 := router.Group("/api/v1")
+	v1 := router.Group(basePath)
 	{
 		v1.GET("/opening", handler.ShowOpeningHandler)
 		v1.POST("/opening", handler.CreateOpeningHandler)
@@ -18,5 +24,8 @@ func initializeRoutes(router *gin.Engine) {
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
 		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
+
+	// Initialize Swagger
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 }
