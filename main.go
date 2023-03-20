8f46b30a53df5faddc03fc22b1fc706b2d621eb4
Daniel Rocha <dgustavo@id.uff.br>
2023-03-19 21:00:08 -0300

    feat: initialize config package

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import (router "github.com/danielbrazrocha/goportunities/router")
+import (
+	"fmt"
+
+	"github.com/danielbrazrocha/goportunities/config"
+	router "github.com/danielbrazrocha/goportunities/router"
+)
 
 func main() {
+
+	//Initialize Configs
+	err := config.Init()
+	if err != nil {
+		fmt.Println(err)
+		//panic(err)
+		return
+	}
+
+	//Initialize router
 	router.Initialize()
 }
